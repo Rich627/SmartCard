fb264c9604b8b0273d40ecf4276f4d254d7eb4e2
Rich627 <rich.liu0912@hotmail.com>
2025-12-15 18:55:56 -0700

    Initial Commit

---
+++ SmartCard/SmartCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCardApp.swift
+//  SmartCard
+//
+//  Created by Rich on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
